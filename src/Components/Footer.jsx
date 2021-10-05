2c70f0999985bbe5edf1f466d01642f067765e8d
irsyadadl <irsyadpanjaitan@gmail.com>
2021-10-05 21:10:13 +0700

    wip

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
     return (
         <footer className="border-t bg-gray-50 pt-16 pb-12 sm:pt-20 md:pt-24 xl:pt-32 sm:pb-20 mt-10">
             <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-                <ul className="Footer_nav__2rFid text-sm font-medium pb-14 sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+                <ul className="text-sm font-medium pb-14 sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
                     <li className="space-y-5 row-span-2">
                         <h2 className="text-xs font-semibold tracking-wide text-gray-900 uppercase">Getting started</h2>
                         <ul className="space-y-4">
